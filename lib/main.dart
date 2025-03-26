5a07944e946ddfad6d319e5ba46c1651b57121e0
Dmitry Krivoruchko <144147209+UmbrellaLeaf5@users.noreply.github.com>
2025-03-27 02:47:14 +0300

    add primary functionality and style

--- lib/main.dart
+++ lib/main.dart
@@ -11,10 +11,11 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Notes organizer",
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      theme: ThemeData.dark(useMaterial3: true).copyWith(
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: Colors.deepPurple, brightness: Brightness.dark),
       ),
+      debugShowCheckedModeBanner: false,
       home: const MainHomePage(),
     );
   }
@@ -28,12 +29,85 @@ class MainHomePage extends StatefulWidget {
 }
 
 class _MainHomePageState extends State<MainHomePage> {
+  /// MEANS: список всех заметок (их содержаний)
+  final List<String> _notes = [];
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Column(mainAxisAlignment: MainAxisAlignment.center),
+    return Scaffold(
+      // IDK: возможно, потом сделаю покрасивее
+      appBar: AppBar(
+        title: const Text("Notes Organizer"),
       ),
+      body: _notes.isEmpty
+          // если заметок нет, просто выводим текст по центру об этом
+          ? const Center(child: Text("No notes yet..."))
+          // если они есть, отображаем с помощью ListView
+          : ListView.builder(
+              itemCount: _notes.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  margin: const EdgeInsets.all(4.0),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Text(_notes[index]),
+                  ),
+                );
+              },
+            ),
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.only(left: 10),
+        child: FloatingActionButton(
+          onPressed: () {
+            _showAddNoteDialog(context);
+          },
+          tooltip: "Add Note",
+          backgroundColor: Colors.grey[800],
+          child: const Icon(Icons.add),
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.startFloat,
+    );
+  }
+
+  /// DOES: выводит на экран диалоговое окно создания новой заметки
+  Future<void> _showAddNoteDialog(BuildContext context) async {
+    String newNote = "";
+
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text("Add a New Note"),
+          content: TextField(
+            autofocus: true,
+            decoration: const InputDecoration(hintText: "Enter your note here"),
+            onChanged: (value) {
+              newNote = value;
+            },
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text("Cancel"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: const Text("Add"),
+              onPressed: () {
+                // добавляем только, если есть какой-то текст
+                if (newNote.isNotEmpty) {
+                  setState(() {
+                    _notes.add(newNote);
+                  });
+                  Navigator.of(context).pop();
+                }
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 }
